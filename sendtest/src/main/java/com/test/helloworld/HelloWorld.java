d347c70a724abedce28f06b95728618b1f7e8603
zhaojb <1214379009@qq.com>
2016-09-20 11:14:45 +0200

    branch

--- sendtest/src/main/java/com/test/helloworld/HelloWorld.java
+++ sendtest/src/main/java/com/test/helloworld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("not bbb HelloWorld");
 		System.out.println("not aaaHelloWorld");
+		System.out.println("I'm sending a msg to branch");
 	}
 }
